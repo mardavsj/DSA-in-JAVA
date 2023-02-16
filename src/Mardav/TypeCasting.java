1a8898af36fbc9a8a4b0b3da3fb16bf80e5b19ef
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-02-16 11:58:45 +0530

    casting

--- src/Mardav/TypeCasting.java
+++ src/Mardav/TypeCasting.java
@@ -1,21 +1,35 @@
 package Mardav;
 
+import java.util.Scanner;
+
 public class TypeCasting {
-    public static void main(String[] args) {
-//        float num = input.nextFloat();
-//        System.out.println(num);
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        float num = input.nextFloat();
+        System.out.println(num);
 
-//        int num = (int)(67.50f);
-//        System.out.print(num);
+        int num1 = (int)(67.50f);
+        System.out.print(num1);
 
-//        byte a =40;
-//        byte b =50;
-//        byte c =100;
-//        int d =a*b/c;
-//        System.out.println(d);
+        byte a =40;
+        byte b =50;
+        byte c =100;
+        int d =a*b/c;
+        System.out.println(d);
 
         int number = 'A';
         System.out.println(number);
         System.out.println("Namaskaram!");
+
+        byte by = 42;
+        char ch = 'a';
+        short s = 1024;
+        int i = 50000;
+        float f = 5.654f;
+        double db = 0.1234;
+        double result = (f * by) + (f / ch) - (db - s + i);
+        System.out.println((f * by) + "" + (f / ch) + "" + (db - s));
+        System.out.print(result);
+
     }
 }
