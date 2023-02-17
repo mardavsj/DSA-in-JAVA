168a4e7aa40630b6b7f1b3f28f17f1991e304b5f
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-02-17 14:51:01 +0530

    casting

--- src/JavaBasics/Loops.java
+++ src/JavaBasics/Loops.java
@@ -3,17 +3,32 @@ import java.util.Scanner;
 
 public class Loops {
     public static void main(String[] args) {
-        // Print odd numbers from 1 to 10.
+        // Print odd numbers from 1 to 10 using for loop.
         for (int num = 1; num<=10; num+=2) {
             System.out.println(num);
         }
-        // Print numbers based on user input.
+
+        // Print numbers based on user input using for loop.
         Scanner input = new Scanner (System.in);
         System.out.print("Enter numbers to be shown: ");
         int n = input.nextInt();
         for (int i = 1; i <= n ; i++) {
             System.out.print(i+ " ");
-            System.out.print("Hello World!  ");
+            System.out.println("Hello World!  ");
         }
+
+        // Print odd numbers from 1 to 20 using while loop.
+        int number = 1;
+        while (number <= 20) {
+            System.out.println(number);
+            number += 2;
+        }
+
+        // Print using do-while loop.
+        int a = 1;
+        do {
+            System.out.println("Same again, Hello World");
+        }
+        while (a != 1);
     }
 }
