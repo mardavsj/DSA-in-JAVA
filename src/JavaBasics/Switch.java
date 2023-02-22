6273988829500c1269ee6a7ec0825c4fe5850ccd
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-02-22 16:20:00 +0530

    casting

--- src/JavaBasics/Switch.java
+++ src/JavaBasics/Switch.java
@@ -13,5 +13,16 @@ public class Switch {
             case "orange" -> System.out.println("Veins filled with Vitamin C");
             default -> System.out.println("Please enter either mango or apple, or you can choose orange as well.");
         }
+
+        int day = input.nextInt();
+        switch (day) {
+            case 1 -> System.out.println("Monday!");
+            case 2 -> System.out.println("Tuesday!");
+            case 3 -> System.out.println("Wednesday!");
+            case 4 -> System.out.println("Thursday!");
+            case 5 -> System.out.println("Friday!");
+            case 6 -> System.out.println("Saturday!");
+            case 7 -> System.out.println("Sunday!");
+        }
     }
 }
