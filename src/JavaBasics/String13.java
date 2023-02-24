003f42b85e4de6f8cbcebdf9f0048e07f39698c1
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-02-24 18:24:01 +0530

    string

---
+++ src/JavaBasics/String13.java
@@ -0,0 +1,13 @@
+package JavaBasics;
+
+public class String13 {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet () {
+        String greeting = "Hey! Welcome home.";
+        return greeting;
+    }
+}
