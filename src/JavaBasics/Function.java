5749a0bb76d1c446ec89f849db9d527e8d84595a
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-02-24 17:58:23 +0530

    methods

--- src/JavaBasics/Function.java
+++ src/JavaBasics/Function.java
@@ -4,9 +4,22 @@ import java.util.Scanner;
 
 public class Function {
     public static void main(String[] args) {
-        Sum();
+        int ans = sums();
+        System.out.println(ans);
+        sum();
     }
-    static void Sum(){
+
+    static int sums () {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        int num3 = input.nextInt();
+        System.out.print("Enter the second number:");
+        int num4 = input.nextInt();
+        int sums = num3+num4;
+        return sums;
+    }
+
+    static void sum(){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the first number:");
         int num1 = input.nextInt();
