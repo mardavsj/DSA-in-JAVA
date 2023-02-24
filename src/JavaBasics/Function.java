843b4f20171fe3df8eb5a8bee40c39aa308b47fe
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-02-24 17:29:23 +0530

    casting

---
+++ src/JavaBasics/Function.java
@@ -0,0 +1,18 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Function {
+    public static void main(String[] args) {
+        Sum();
+    }
+    static void Sum(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        int num1 = input.nextInt();
+        System.out.print("Enter the second number:");
+        int num2 = input.nextInt();
+        int sum = num1+num2;
+        System.out.println("The sum will be "+ sum);
+    }
+}
