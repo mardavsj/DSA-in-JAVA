3dc69da9f045f43e79870226f500adb59c641192
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-02-24 18:57:12 +0530

    swap

---
+++ src/JavaBasics/Swapping.java
@@ -0,0 +1,14 @@
+package JavaBasics;
+
+public class Swapping {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int swap = a;
+        a = b;
+        b = swap;
+
+        System.out.println(a + " " + b);
+    }
+}
