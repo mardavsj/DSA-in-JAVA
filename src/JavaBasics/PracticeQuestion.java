4349b368cc2bb9ba2c3ca25733250af725c84105
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-02-28 18:31:52 +0530

    prime/non-prime

---
+++ src/JavaBasics/PracticeQuestion.java
@@ -0,0 +1,29 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class PracticeQuestion {
+
+    // Prime and not prime numbers.
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        boolean ans = isPrime(n);
+        System.out.println(ans);
+    }
+
+    static boolean isPrime(int n){
+        if (n<=1) {
+            return false;
+        }
+        int c = 2;
+        while (c * c <= n){
+            if (n % c == 0){
+                return false;
+            }
+            c++;
+        }
+        return c * c > n;
+    }
+}
