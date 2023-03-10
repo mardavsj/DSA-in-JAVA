d6ce0256d93e98b2e8d20cc67fd3128c9b103ec9
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-03-10 16:11:10 +0530

    arrays basic

--- Data-Structures/src/Arrays/Basic.java
+++ Data-Structures/src/Arrays/Basic.java
@@ -1,5 +1,23 @@
 package Arrays;
 
 public class Basic {
+    public static void main(String[] args) {
+        // Store a number.
+        int a = 19;
+        System.out.println(a);
 
+        // Store a name.
+        String name = "Mardav Jadaun";
+        System.out.println(name);
+
+        // Store 10 numbers in array.
+
+        // 1st method:
+        int[] roll = new int[10];
+        System.out.println(roll);
+
+        // 2nd method:
+        int[] Rollno = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(Rollno);
+    }
 }
