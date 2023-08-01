0c4d908ace611d93ae7887c1e83d0ab392140405
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-08-01 20:50:37 +0530

    system date & time

---
+++ src/JavaBasics/PracticeQ6.java
@@ -0,0 +1,7 @@
+package JavaBasics;
+
+public class PracticeQ6 {
+    public static void main(String[] args){
+        System.out.format("\nCurrent Date time: %tc%n\n", System.currentTimeMillis());
+    }
+}
