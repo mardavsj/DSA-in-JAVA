ee2e679fb598afe24d474f28d3e48debac7e06a6
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-08-01 20:51:10 +0530

    system date & time

--- src/JavaBasics/PracticeQ6.java
+++ src/JavaBasics/PracticeQ6.java
@@ -1,5 +1,6 @@
 package JavaBasics;
 
+// Display the system time
 public class PracticeQ6 {
     public static void main(String[] args){
         System.out.format("\nCurrent Date time: %tc%n\n", System.currentTimeMillis());
