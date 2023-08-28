ea62dd5f2bb7008644767431a3abc725b81d4ddc
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-08-28 16:20:13 +0530

    Greetings

---
+++ src/JavaPrograms/HelloWorld.java
@@ -0,0 +1,8 @@
+// WAP to print a message "Hello World".
+
+package JavaPrograms;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
